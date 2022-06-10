5fc0430cbcac7d61aac4ccc460ade7ecf9a24213
Lucas Trognon <lucas.trognon@gmail.com>
2022-06-10 15:36:09 +0200

    4 bytes were saved

--- robot/handlers/soup.py
+++ robot/handlers/soup.py
@@ -92,7 +92,7 @@ def soup(update, context):
     inputs = context.args
     results: List[Dish] = []
 
-    if len(inputs) > 0:
+    if len(inputs):
         price_thrsh = float(inputs[0])
     else:
         price_thrsh = 10  # Default budget is 10 CHF
